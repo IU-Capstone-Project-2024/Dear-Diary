8f7e1f25d5fb4a056c5fd2387ea05b8bc3a96e36
AndrewLevada <levada.andrew@gmail.com>
2024-07-04 12:39:10 +0300

    added image storage logic

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,45 @@
+import datetime
+import random
+from typing import Optional
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from llm import generate_emotion, generate_recommendation_for_emotion, generate_response_to_note, generate_note_title
 from utils import sanityze_text_letters_only, sanityze_text_no_special_chars
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
 
 load_dotenv()
 app = FastAPI()
+
+cred = credentials.Certificate("firebase-key.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'capstone-dear-diary.appspot.com'
+})
+
+bucket = storage.bucket()
+
+
+class GetNoteCoverBody(BaseModel):
+    image_id: str | None = None
+
+
+class GetNoteCoverResponse(BaseModel):
+    imageUrl: str
+    imageId: str
+
+
+@app.put("/noteCover")
+async def note_cover(body: GetNoteCoverBody) -> GetNoteCoverResponse:
+    image_id = body.image_id
+
+    if image_id is None:
+        image_id = str(random.randint(1, 80))
+
+    blob = bucket.blob(f"note_covers/{image_id}.jpg")
+    url = blob.generate_signed_url(expiration=datetime.timedelta(days=60), method='GET')
+    return GetNoteCoverResponse(imageUrl=url, imageId=image_id)
 
 
 class StatusResponse(BaseModel):
